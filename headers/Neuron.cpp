3bf3457e599b04bb829e20f25df646005429013e
OmarSulaivany <omar.taha.mohammed@gmail.com>
2021-01-16 21:36:13 +0300

    DOW

--- headers/Neuron.cpp
+++ headers/Neuron.cpp
@@ -54,3 +54,22 @@ void Neuron::feedforward(const layer &preLayer)
     /* Apply an activation function to the output to make it between a specific range, in our case -1,1. */
 	m_outputVal = Neuron::Activation(sum);
 }
+
+void Neuron::calcOutputGradients(double targetVals)
+{	
+	/* Calculate the difference between the target and output value of the neuron. */
+	double delta = targetVals - m_outputVal;
+
+	/* To get the gradient we multiply the derivative of the activation function with our delta change. */
+	m_gradient = delta * Neuron::Activation_prime(m_outputVal);
+}
+
+void Neuron::calcHiddenGradients(const Layer& nextLayer)
+{
+	/* Because the hidden layer doesn't have the Target value,and to calculate the error rate we will take the sum of the
+	   derivative of weight of the next layer. */
+	double dow = sumDOW(nextLayer);
+
+	/* To get the gradient we multiply the derivative of the activation function with our delata change. */
+	m_gradient = dow * Neuron::activationDerivative(m_outputVal);
+}
